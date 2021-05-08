3c1f6962471986c542673fcedb7baaad0672926f
xSebeq <sebeq245@interia.eu>
2021-05-08 15:37:28 +0200

    added models

--- Biblioteka/models.py
+++ Biblioteka/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from softdelete.models import SoftDeleteObject
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Book(SoftDeleteObject):
+    MIEKKA = "miekka"
+    TWARDA = "twarda"
+    TYPE_OF_BOOK_CHOICES = [
+        (MIEKKA, "Miękka"),
+        (TWARDA, "Twarda"),
+    ]
+    tytul = models.TextField()
+    autor = models.TextField()
+    typ = models.CharField(
+        max_length=6,
+        choices=TYPE_OF_BOOK_CHOICES,
+    )
+    wydawnictwo = models.TextField()
+    data_premiery = models.DateField()
+    data_publikacji = models.DateField(default=None, null=True, blank=True)
+    liczba_stron = models.IntegerField()
+    uzytkownik = models.ForeignKey(User, on_delete=models.CASCADE)
+    zdjecie = models.ImageField()
+
+    def __str__(self):
+        return self.tytul
